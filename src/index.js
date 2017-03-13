1baf2c547b872bbda900d839a57ff6b72dc4f6e9
gcguevara <garrettcguevara@gmail.com>
2017-03-13 15:55:39 -0700

    Explore setting state.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import API_KEY from '../check';
+import SearchBar from "./components/search_bar";
 
 
 // Create a new component, this component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Take this component's generated HTML and put it in the DOM
